89ed77393a3baff7c2124bbd7a394b3d6403f7df
Reader <Reader>
2022-11-22 13:27:45 -0800

    adding theme, locale, and module.xml

--- data/emptyProject/src/Main.hx
+++ data/emptyProject/src/Main.hx
@@ -1,10 +1,16 @@
 package ;
+package;
 
 import haxe.ui.HaxeUIApp;
+import haxe.ui.locale.LocaleManager;
+import haxe.ui.Toolkit;
 
 class Main {
     public static function main() {
         var app = new HaxeUIApp();
+		// Toolkit.autoScale = true;
+		Toolkit.theme = "joedark"; // TODO DO stuff
+		LocaleManager.instance.language = "en";
         app.ready(function() {
             app.addComponent(new MainView());
 
